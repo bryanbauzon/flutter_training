f7f48dd924e5e2acbdc13afa13f60d6453c68c9c
bryanbauzon <mxbrynbzn.8@gmail.com>
2020-03-14 10:49:32 +0800

    Added folder screens | removed some codes | added traing-page.dart

---
+++ lib/screens/training-page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class TraingPage extends StatefulWidget{
+  @override
+  _TrainingPageState createState() => _TrainingPageState();
+}
+
+class _TrainingPageState extends State<TraingPage>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:Text("Flutter Training"),
+      ),
+    );
+  }
+
+}
