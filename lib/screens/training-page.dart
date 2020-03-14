08cf47339fc1a438d9478ede210d5774384e287b
bryanbauzon <mxbrynbzn.8@gmail.com>
2020-03-14 12:36:29 +0800

    added card

--- lib/screens/training-page.dart
+++ lib/screens/training-page.dart
@@ -12,27 +12,68 @@ class _TrainingPageState extends State<TraingPage>{
       appBar: AppBar(
         title:Text("Flutter Training"),//develop
       ),
-      body: Center(
-        child: Container(
+      body: Container(
            margin: EdgeInsets.all(10),
            padding: EdgeInsets.all(10),
            child: Column(
              crossAxisAlignment:CrossAxisAlignment.center,
              mainAxisAlignment:MainAxisAlignment.center,
                 children: <Widget>[
+                  
                   //Username
                   TextField(
                     decoration:InputDecoration(hintText: "Username")
                   ),
+                  
                   //Password
                   TextField(
                     obscureText:true,
                     decoration:InputDecoration(hintText:"Password")
-                  )
+                  ),
+                  
+                  // Login Button
+                  FlatButton(
+                    onPressed: (){},
+                    color: Colors.blue,
+                    textColor: Colors.white,
+                    padding: EdgeInsets.all(5),
+                    splashColor: Colors.blueAccent,
+                    child: Text("Flat Button",style: TextStyle(fontSize: 18.0),),
+                  ),
+                  const SizedBox(height: 20,),
+                   RaisedButton(
+                     onPressed: (){},
+                      textColor: Colors.white,
+                      color: Colors.blueAccent,
+                      child: Text("Raised Button"),
+                  ),
+                  Card(
+                      child: Column(
+                        mainAxisSize:MainAxisSize.min,
+                        children:<Widget>[
+                          const ListTile(
+                            leading: Icon(Icons.add_a_photo),
+                            title:Text("Photography"),
+                            subtitle: Text("Max Bryan C. Bauzon"),
+                          ),
+                          ButtonBar(
+                            children:<Widget>[
+                                FlatButton(
+                                  onPressed: (){}, 
+                                  child: const Text("View")
+                                ),
+                                 FlatButton(
+                                  onPressed: (){}, 
+                                  child: const Text("Add")
+                                )
+                            ]
+                          ),
+                        ]
+                      ) ,
+                  ), 
                 ],
            ),
         ),
-      ),
     );
   }
 
