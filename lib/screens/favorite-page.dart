d910da0333c130bb6742c22db063b369f90be342
bryanbauzon <mxbrynbzn.8@gmail.com>
2020-03-19 12:08:42 +0800

    Added favorite feature

--- lib/screens/favorite-page.dart
+++ lib/screens/favorite-page.dart
@@ -7,17 +7,142 @@ class FavoriteWidget extends StatefulWidget{
 }
 
 class _FavoriteWidgetState extends State<FavoriteWidget>{
+
+  bool _isFavorited = false;
+  int _favoriteCount = 41;
+
+  Widget _titleSection = Container(
+    padding: const EdgeInsets.all(32),
+    child: Row(
+      children: <Widget>[
+        Expanded(
+          /*1*/
+          child:Column(
+            crossAxisAlignment:CrossAxisAlignment.start,
+            children: <Widget>[
+              /*2*/
+              Container(
+                padding: const EdgeInsets.only(bottom:8),
+                child:Text(
+                  'Oeschinen Lake Campground',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold
+                  )
+                )
+              ),
+              Text(
+                'Kandersteg, Switzerland',
+                style: TextStyle(
+                  color:Colors.grey[500],
+                ),                
+              )
+            ],
+          ),
+          ),
+          Icon(
+            Icons.favorite,
+            color:Colors.red[500] 
+          ),
+          Text('41')
+      ],
+    ),
+  );
+ Column _buildButtonColumn(Color color, IconData icon, String label){
+      return Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Icon(icon,color:color),
+          Container(
+            margin:const EdgeInsets.only(top:8),
+            child:Text(
+                label,
+                style: TextStyle(
+                  fontSize:12,
+                  fontWeight:FontWeight.w400,
+                  color:color
+                ),
+            )
+          )
+        ],
+      );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body:Center(
-        child: Column(
-          children: <Widget>[
-             Text("Favorites")
-          ],
+    Color color = Theme.of(context).primaryColor;
+
+Widget _buttonSection = Container(
+    child:Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          _buildButtonColumn(color,Icons.call,'Call'),
+           _buildButtonColumn(color,Icons.near_me,'Route'),
+            _buildButtonColumn(color,Icons.share,'Share'),  
+        ],
+    )
+  );
+
+  Widget _textSection = Container(
+    padding: const EdgeInsets.all(32),
+    child: Text('Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+        'Alps. Situated 1,578 meters above sea level, it is one of the '
+        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+        'half-hour walk through pastures and pine forest, leads you to the '
+        'lake, which warms to 20 degrees Celsius in the summer. Activities '
+        'enjoyed here include rowing, and riding the summer toboggan run.',
+        softWrap: true,
         ),
+
+  );
+    return Scaffold(
+      body:ListView(
+          children: <Widget>[
+            Image.asset(
+              'images/lake.jpg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
+           _titleSection,
+           _buttonSection,
+           _textSection
+          ],
         )
     );
   }
+ 
+
+
+  void _toggleFavorite(){
+  setState((){
+      if(_isFavorited){
+          _favoriteCount -= 1;
+          _isFavorited = false;
+      }else{
+          _favoriteCount += 1;
+          _isFavorited = true;
+      }
+  });
 }
+}
+
+
+//  Container(
+//                 padding: EdgeInsets.all(0),
+//                 child: IconButton(
+//                   icon:_isFavorited?  Icon(Icons.favorite):Icon(Icons.favorite_border),
+//                   color: Colors.red[500],  
+//                   onPressed: _toggleFavorite,
+//                 ),
+//             ),
+//             SizedBox(
+//               width: 18,
+//               child: Container(
+//                 child:Text('$_favoriteCount'),
+//               ),
+
+//             )
+
+
 
